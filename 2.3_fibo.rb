234eb52d058330050447a801744fc0a9b9748b1f
Ivan Macbook <filin87@gmail.com>
2020-10-22 15:29:06 +0300

    change while to loop

--- 2.3_fibo.rb
+++ 2.3_fibo.rb
@@ -2,18 +2,13 @@ arr = []
 fib1 = 1
 fib2 = 0
 
-# while fib1 < 100 do
-#   fib_sum = fib1 + fib2
-#   fib1 = fib2
-#   fib2 = fib_sum
-#   arr << fib1 if fib1 < 100
-# end
-
-begin
+loop do
   fib_sum = fib1 + fib2
   fib1 = fib2
+
+  break if fib1 > 100
+
   fib2 = fib_sum
   arr << fib1 
-end while fib1 < 89 #Пока вижу только один способ обойтись одним условием - понизить проверочное число.
-                    #Ну, еще я только-что узнал новую конструкицю с условием в конце. Решил применить. 
+end
 puts arr
