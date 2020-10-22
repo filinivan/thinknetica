b460cf2569c5062b1540ca4eb58aaa031832708d
Ivan Macbook <filin87@gmail.com>
2020-10-22 16:27:10 +0300

    Last changes

--- 2.4_alphabet.rb
+++ 2.4_alphabet.rb
@@ -2,10 +2,8 @@ hash = {}
 alphabet = ('а'..'я').to_a #Генерация не включает букву "ё". Если её наличие принципиально - массив придётся заполнить руками.
 vowel = ['а', 'у', 'о', 'ы', 'и', 'э', 'я', 'ю', 'ё', 'е',]
 
-alphabet.each.with_index(1) do |w|
-  if vowel.include?(w)
-    hash[w] = alphabet.index(w)
-  end
+alphabet.each.with_index(1) do |w, index|
+  hash[w] = index if vowel.include?(w)
 end
 
 puts hash
